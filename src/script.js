fc0cfe410c05dc25a8f04b8317ec0b9d754febd5
Ilya Yudin <nibezo.cs@gmail.com>
2022-04-22 01:06:14 +0300

    js: feat: user can insert his own telegram link

--- src/script.js
+++ src/script.js
@@ -1,9 +1,9 @@
-// IF Enter pressed, do:
 document.addEventListener('keydown', function(event) {
     if (event.code == 'Enter') {
       var input = document.getElementById("input-link");
       if (input && input.value) {
-
+        let inputValue = document.getElementById("input-link").value;
+        inputValue = inputValue.substring(13);
         document.getElementById('input-link').style.visibility = "hidden";
         document.getElementById('input-dropzone').style.visibility = "hidden";
         
@@ -12,7 +12,7 @@ document.addEventListener('keydown', function(event) {
         
         document.getElementById("widget").setAttribute("sync", "");
         document.getElementById("widget").setAttribute("src", "https://telegram.org/js/telegram-widget.js?19");
-        document.getElementById("widget").setAttribute("data-telegram-post", "denissexy/5633");
+        document.getElementById("widget").setAttribute("data-telegram-post", inputValue);
         document.getElementById("widget").setAttribute("data-width", "100%");
         
         
