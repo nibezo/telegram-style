4a5bf3127ea0af45524e734b3ca5c3ce98da6836
AleevTimur <aleev.time@gmail.com>
2022-04-22 14:53:38 +0300

    fix: change place of post(place script with post in container)

--- src/script.js
+++ src/script.js
@@ -1,24 +1,22 @@
-document.addEventListener('keydown', function(event) {
-    if (event.code == 'Enter') {
-      let input = document.getElementById("input-link");
-      if (input && input.value) {
-        let inputValue = document.getElementById("input-link").value;
-        let scriptWidget = document.getElementById("widget");
-        
-        inputValue = inputValue.substring(13);
-        document.getElementById('input-link').style.visibility = "hidden";
-        document.getElementById('input-dropzone').style.visibility = "hidden";
-        
-        const element = document.getElementById("input-dropzone");
-        element.remove();
-        
-        scriptWidget.setAttribute("sync", "");
-        scriptWidget.setAttribute("src", "https://telegram.org/js/telegram-widget.js?19");
-        scriptWidget.setAttribute("data-telegram-post", inputValue);
-        scriptWidget.setAttribute("data-width", "100%");
-      }
-      else {
-        alert('Please, enter a link!');
-      }
+document.addEventListener("keydown", function (event) {
+  if (event.code == "Enter") {
+    let input = document.getElementById("input-link");
+    if (input && input.value) {
+      let inputValue = document.getElementById("input-link").value;
+      let scriptWidget = document.getElementById("widget");
+
+      inputValue = inputValue.substring(13);
+      document.getElementById("input-link").style.visibility = "hidden";
+
+      scriptWidget.setAttribute("sync", "");
+      scriptWidget.setAttribute(
+        "src",
+        "https://telegram.org/js/telegram-widget.js?19"
+      );
+      scriptWidget.setAttribute("data-telegram-post", inputValue);
+      scriptWidget.setAttribute("data-width", "100%");
+    } else {
+      alert("Please, enter a link!");
     }
-  });
+  }
+});
