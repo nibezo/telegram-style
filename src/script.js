92bc7370b26e4d00e1ddd553fb7da6ff006826f0
Ilya Yudin <nibezo.cs@gmail.com>
2022-04-24 22:55:22 +0300

    feat: add input-dropzone back.color changing

--- src/script.js
+++ src/script.js
@@ -14,17 +14,44 @@ document.addEventListener("keydown", function (event) {
       scriptWidget.setAttribute("data-telegram-post", inputValue);
       scriptWidget.setAttribute("data-width", "100%");
       document.getElementById("input-dropzone").appendChild(scriptWidget);
+      // document.getElementsById("input-dropzone").style.backgroundColor = "100px";
+      
     } else {
-        alert("Please, enter a link!");
-      }
+      alert("Please, enter a link!");
     }
-  });
+  }
+});
+
+function resetPost() {
+  let element = document.getElementsByTagName("iframe"), index;
+  for (index = element.length - 1; index >= 0; index--) {
+    element[index].parentNode.removeChild(element[index]);
+  }
+  document.getElementById("widget").remove();
+  document.getElementById("input-link").style.visibility = "visible";
   
-  function resetPost() {
-    let element = document.getElementsByTagName("iframe"), index;
-    for (index = element.length - 1; index >= 0; index--) {
-      element[index].parentNode.removeChild(element[index]);
-    }
-    document.getElementById("widget").remove();
-    document.getElementById("input-link").style.visibility = "visible";
+}
+
+function firstButtonClick() {
+  document.getElementById("input-dropzone").style.backgroundImage = "linear-gradient(to bottom right, #fd6585,rgb(255, 211, 165)";
+}
+
+function secondButtonClick() {
+  document.getElementById("input-dropzone").style.backgroundImage = "linear-gradient(to bottom right, #00dbde, #fc00ff)";
+}
+
+function thirdButtonClick() {
+  document.getElementById("input-dropzone").style.backgroundImage = "linear-gradient(to bottom, #c084fc, #6366f1)";
+}
+
+function fourthButtonClick() {
+  document.getElementById("input-dropzone").style.backgroundImage = "linear-gradient(to bottom, #ed6ea0, #ec8c69)";
+}
+
+function fifthButtonClick() {
+  document.getElementById("input-dropzone").style.backgroundImage = "linear-gradient(to bottom right, #f9a8d4, #f472b6, #f87171)";
+}
+
+function sixthButtonClick() {
+  document.getElementById("input-dropzone").style.backgroundImage = "linear-gradient(to bottom right, #84fab0, #8fd3f4)";
 }
