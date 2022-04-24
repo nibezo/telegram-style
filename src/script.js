4aeb4d2770071cbaa56deb2fe080e666c1719f94
Ilya Yudin <nibezo.cs@gmail.com>
2022-04-24 21:53:37 +0300

    js: feat: full functionality of reset button

--- src/script.js
+++ src/script.js
@@ -1,29 +1,28 @@
 document.addEventListener("keydown", function (event) {
   if (event.code == "Enter") {
     let input = document.getElementById("input-link");
+    
     if (input && input.value) {
       let inputValue = document.getElementById("input-link").value;
-      let scriptWidget = document.getElementById("widget");
-
       inputValue = inputValue.substring(13);
       document.getElementById("input-link").style.visibility = "hidden";
       
-      scriptWidget.setAttribute("sync", "");
-      scriptWidget.setAttribute(
-        "src",
-        "https://telegram.org/js/telegram-widget.js?19"
-        );
-        scriptWidget.setAttribute("data-telegram-post", inputValue);
-        scriptWidget.setAttribute("data-width", "100%");
-      } else {
+      let scriptWidget = document.createElement('script');
+      scriptWidget.setAttribute("async", "");
+      scriptWidget.setAttribute("src", "https://telegram.org/js/telegram-widget.js?19");
+      scriptWidget.setAttribute("data-telegram-post", inputValue);
+      scriptWidget.setAttribute("data-width", "100%");
+      document.getElementById("input-dropzone").appendChild(scriptWidget);
+    } else {
         alert("Please, enter a link!");
       }
     }
   });
-
+  
   function resetPost() {
     document.getElementById("input-link").style.visibility = "visible";
-    document.getElementsByTagName("script")[0].removeAttribute("sync"); 
+    
+    document.getElementsByTagName("script")[0].removeAttribute("async"); 
     document.getElementsByTagName("script")[0].removeAttribute("src"); 
     document.getElementsByTagName("script")[0].removeAttribute("data-telegram-post"); 
     document.getElementsByTagName("script")[0].removeAttribute("data-width");
