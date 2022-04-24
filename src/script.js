be70794c6c8ab292a5152dd8962e173fe40afb5b
Ilya Yudin <nibezo.cs@gmail.com>
2022-04-24 13:42:04 +0300

    js: feat: delete iframe with telegram widget

--- src/script.js
+++ src/script.js
@@ -7,24 +7,29 @@ document.addEventListener("keydown", function (event) {
 
       inputValue = inputValue.substring(13);
       document.getElementById("input-link").style.visibility = "hidden";
-
+      
       scriptWidget.setAttribute("sync", "");
       scriptWidget.setAttribute(
         "src",
         "https://telegram.org/js/telegram-widget.js?19"
-      );
-      scriptWidget.setAttribute("data-telegram-post", inputValue);
-      scriptWidget.setAttribute("data-width", "100%");
-    } else {
-      alert("Please, enter a link!");
+        );
+        scriptWidget.setAttribute("data-telegram-post", inputValue);
+        scriptWidget.setAttribute("data-width", "100%");
+      } else {
+        alert("Please, enter a link!");
+      }
     }
-  }
-});
+  });
 
-function resetPost() {
-  document.getElementById("input-link").style.visibility = "visible";
-  document.getElementsByTagName("script")[0].removeAttribute("sync"); 
-  document.getElementsByTagName("script")[0].removeAttribute("src"); 
-  document.getElementsByTagName("script")[0].removeAttribute("data-telegram-post"); 
-  document.getElementsByTagName("script")[0].removeAttribute("data-width");
+  function resetPost() {
+    document.getElementById("input-link").style.visibility = "visible";
+    document.getElementsByTagName("script")[0].removeAttribute("sync"); 
+    document.getElementsByTagName("script")[0].removeAttribute("src"); 
+    document.getElementsByTagName("script")[0].removeAttribute("data-telegram-post"); 
+    document.getElementsByTagName("script")[0].removeAttribute("data-width");
+    var element = document.getElementsByTagName("iframe"), index;
+
+    for (index = element.length - 1; index >= 0; index--) {
+      element[index].parentNode.removeChild(element[index]);
+    }
 }
