5eb0819124bbd5ce18baa1c9554b67f0e21b5d39
AleevTimur <aleev.time@gmail.com>
2022-04-25 14:42:11 +0300

    fix: change some names and add widget container

--- src/script.js
+++ src/script.js
@@ -18,7 +18,14 @@ document.addEventListener("keydown", function (event) {
       );
       scriptWidget.setAttribute("data-telegram-post", inputValue);
       scriptWidget.setAttribute("data-width", "100%");
-      document.getElementById("input-dropzone").appendChild(scriptWidget);
+
+      let widgetContainer = document.createElement("div");
+      widgetContainer.id = "widget-container";
+      widgetContainer.classList.add("widget-container");
+      let widgetSideElement = document.getElementById("widget-side");
+      widgetSideElement.appendChild(widgetContainer);
+      widgetContainer.appendChild(scriptWidget);
+
       backgroundGradient = true;
       firstButtonClick();
     } else {
@@ -33,56 +40,56 @@ function resetPost() {
   for (index = element.length - 1; index >= 0; index--) {
     element[index].parentNode.removeChild(element[index]);
   }
-  document.getElementById("widget").remove();
+  document.getElementById("widget-container").remove();
   document.getElementById("input-link").style.display = "block";
-  document.getElementById("input-dropzone").style.backgroundImage = "";
+  document.getElementById("widget-container").style.backgroundImage = "";
   backgroundGradient = false;
 }
 
 function firstButtonClick() {
   if (backgroundGradient === true) {
-    document.getElementById("input-dropzone").style.backgroundImage =
+    document.getElementById("widget-container").style.backgroundImage =
       "linear-gradient(to bottom right, #fd6585,rgb(255, 211, 165)";
-    document.getElementById("input-dropzone").style.borderRadius = "10px";
+    document.getElementById("widget-container").style.borderRadius = "10px";
   }
 }
 
 function secondButtonClick() {
   if (backgroundGradient === true) {
-    document.getElementById("input-dropzone").style.backgroundImage =
+    document.getElementById("widget-container").style.backgroundImage =
       "linear-gradient(to bottom right, #00dbde, #fc00ff)";
-    document.getElementById("input-dropzone").style.borderRadius = "10px";
+    document.getElementById("widget-container").style.borderRadius = "10px";
   }
 }
 
 function thirdButtonClick() {
   if (backgroundGradient === true) {
-    document.getElementById("input-dropzone").style.backgroundImage =
+    document.getElementById("widget-container").style.backgroundImage =
       "linear-gradient(to bottom, #c084fc, #6366f1)";
-    document.getElementById("input-dropzone").style.borderRadius = "10px";
+    document.getElementById("widget-container").style.borderRadius = "10px";
   }
 }
 
 function fourthButtonClick() {
   if (backgroundGradient === true) {
-    document.getElementById("input-dropzone").style.backgroundImage =
+    document.getElementById("widget-container").style.backgroundImage =
       "linear-gradient(to bottom, #ed6ea0, #ec8c69)";
-    document.getElementById("input-dropzone").style.borderRadius = "10px";
+    document.getElementById("widget-container").style.borderRadius = "10px";
   }
 }
 
 function fifthButtonClick() {
   if (backgroundGradient === true) {
-    document.getElementById("input-dropzone").style.backgroundImage =
+    document.getElementById("widget-container").style.backgroundImage =
       "linear-gradient(to bottom right, #f9a8d4, #f472b6, #f87171)";
-    document.getElementById("input-dropzone").style.borderRadius = "10px";
+    document.getElementById("widget-container").style.borderRadius = "10px";
   }
 }
 
 function sixthButtonClick() {
   if (backgroundGradient === true) {
-    document.getElementById("input-dropzone").style.backgroundImage =
+    document.getElementById("widget-container").style.backgroundImage =
       "linear-gradient(to bottom right, #84fab0, #8fd3f4)";
-    document.getElementById("input-dropzone").style.borderRadius = "10px";
+    document.getElementById("widget-container").style.borderRadius = "10px";
   }
 }
